90fc014c31dc0569e592d6029277855a006900e4
Jose A De Jesus <72800461+zaycashh@users.noreply.github.com>
2026-01-21 23:13:39 -0500

    Delete frontend/lang/modules/der/es.js

--- frontend/lang/modules/der/es.js
+++
@@ -1,5 +0,0 @@
-export default {
-  title: "Pruebas de Drogas y Alcohol (DER)",
-  description: "Este módulo capacita a los Representantes Designados del Empleador (DER).",
-  startButton: "Iniciar Módulo"
-};
