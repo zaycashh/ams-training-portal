50bf1a33cca4665a31c187c55bbc9b42fdd1d245
Jose A De Jesus <72800461+zaycashh@users.noreply.github.com>
2026-01-21 21:50:17 -0500

    Create es.js

---
+++ frontend/lang/modules/der/es.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Pruebas de Drogas y Alcohol (DER)",
+  description: "Este módulo capacita a los Representantes Designados del Empleador (DER).",
+  startButton: "Iniciar Módulo"
+};
