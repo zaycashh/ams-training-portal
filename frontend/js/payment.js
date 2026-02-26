ce3440b9214bb48cbf022cba9b0439cc4f7143d3
Jose A De Jesus <72800461+zaycashh@users.noreply.github.com>
2026-02-26 01:04:31 -0500

    Update payment.js

--- frontend/js/payment.js
+++ frontend/js/payment.js
@@ -1,6 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const params = new URLSearchParams(window.location.search);
   const module = params.get("module");
+  const productType = params.get("type");
+  const qty = parseInt(params.get("qty") || "1", 10);
+  const params = new URLSearchParams(window.location.search);
+  const module = params.get("module");
 
   const payBtn = document.getElementById("payNowBtn");
 
@@ -31,4 +35,18 @@ function simulateStripeSuccess(module) {
 
   alert("Payment successful!");
   window.location.href = "dashboard.html";
+}
+
+if (productType === "employee_seats") {
+
+  const company = JSON.parse(localStorage.getItem("companyProfile"));
+
+  if (!company.seats) company.seats = {};
+  if (!company.seats.employee) {
+    company.seats.employee = { total: 0 };
+  }
+
+  company.seats.employee.total += 5;
+
+  localStorage.setItem("companyProfile", JSON.stringify(company));
 }
